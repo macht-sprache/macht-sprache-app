bd8ecfab909e395f4a90c2b868e74f722959c302
Timur Çelikel <me@timurcelikel.com>
2021-01-18 14:41:59 +0100

    layout: add sidebar content

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -3,15 +3,17 @@ import s from './style.module.css';
 type Props = {
     title: string;
     children: React.ReactNode;
+    sidebar?: React.ReactNode;
 };
 
-function Layout({ title, children }: Props) {
+function Layout({ title, children, sidebar }: Props) {
     return (
         <div className={s.container}>
             <div className={s.sidebar}>
                 <div className={s.sidebarInner}>
                     <div className={s.sidebarInnerInner}>
                         <div className={s.logo}>macht.sprache.</div>
+                        {sidebar}
                     </div>
                 </div>
             </div>
