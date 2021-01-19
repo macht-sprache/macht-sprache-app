6982b5b09f53b563edbdd7cd3dba0272b4ee7cbe
Timur Çelikel <me@timurcelikel.com>
2021-01-19 10:43:41 +0100

    add Login and Register Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import Layout from './Layout';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import HomePage from './HomePage';
 import Nav from './Nav';
 import TermPage from './TermPage';
 import ElementTestPage from './ElementTestPage';
+import RegisterPage from './RegisterPage';
+import LoginPage from './LoginPage';
 
 function App() {
     return (
         <Router>
-            <Layout sidebar={<Nav />} topRightMenu={<a href="/">Login</a>}>
+            <Layout
+                sidebar={<Nav />}
+                topRightMenu={
+                    <>
+                        <Link to="/signup">Sign up</Link> <Link to="/login">Login</Link>
+                    </>
+                }
+            >
                 <Switch>
                     <Route path="/" exact>
                         <HomePage />
+                    </Route>
+                    <Route path="/signup" exact>
+                        <RegisterPage />
+                    </Route>
+                    <Route path="/login" exact>
+                        <LoginPage />
                     </Route>
                     <Route path="/term/:termId" exact>
                         <TermPage />
---
+++ src/LoginPage/index.tsx
@@ -0,0 +1,5 @@
+import Header from '../Header';
+
+export default function LoginPage() {
+    return <Header>Login</Header>;
+}
