f6b03cc9fe03496719eb327709ad5b5a7aa92f16
Timur Çelikel <me@timurcelikel.com>
2021-01-19 18:01:44 +0100

    Login: add form

--- src/LoginPage/index.tsx
+++ src/LoginPage/index.tsx
@@ -1,5 +1,39 @@
+import { useState } from 'react';
+import Button, { ButtonContainer } from '../Form/Button';
+import { Input } from '../Form/Input';
+import InputContainer from '../Form/InputContainer';
 import Header from '../Header';
 
 export default function LoginPage() {
-    return <Header>Login</Header>;
+    const [userName, setUserName] = useState('');
+    const [password, setSetPassword] = useState('');
+
+    return (
+        <>
+            <Header>Login</Header>
+            <div style={{ maxWidth: '500px' }}>
+                <InputContainer>
+                    <Input
+                        label="Mail Address"
+                        value={userName}
+                        onChange={value => {
+                            setUserName(value.target.value);
+                        }}
+                    />
+                    <Input
+                        label="Password"
+                        value={password}
+                        type="password"
+                        onChange={value => {
+                            setSetPassword(value.target.value);
+                        }}
+                    />
+                </InputContainer>
+                <ButtonContainer>
+                    <Button>Cancel</Button>
+                    <Button primary={true}>Login</Button>
+                </ButtonContainer>
+            </div>
+        </>
+    );
 }
