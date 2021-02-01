e9a95d645d133571325915955ba7500ec8c91eab
Timur Çelikel <me@timurcelikel.com>
2021-02-01 18:31:25 +0100

    Add TermWithLang
    component to add lang attribute and highlight inline

---
+++ src/TermWithLang/index.tsx
@@ -0,0 +1,14 @@
+import s from './style.module.css';
+
+type TermWithLangProps = {
+    children: React.ReactNode;
+    lang: string;
+};
+
+export function TermWithLang({ children, lang }: TermWithLangProps) {
+    return (
+        <span className={s.term} lang={lang}>
+            {children}
+        </span>
+    );
+}
