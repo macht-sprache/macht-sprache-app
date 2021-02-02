a3c86a901557aa755299ccf1ef1095b3c9d19f35
Timur Çelikel <me@timurcelikel.com>
2021-02-02 18:22:28 +0100

    Header: add subHeading

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -3,13 +3,15 @@ import s from './style.module.css';
 type Props = {
     children: React.ReactNode;
     subLine?: React.ReactNode;
+    subHeading?: React.ReactNode;
     mainLang?: string;
 };
 
-export default function Header({ children, subLine, mainLang }: Props) {
+export default function Header({ children, subLine, mainLang, subHeading }: Props) {
     return (
         <header className={s.header}>
-            <h1 className={s.heading}>
+            {subHeading && <h2 className={s.subHeading}>{subHeading}</h2>}
+            <h1 className={subHeading ? s.headingWithSubheading : s.heading}>
                 <span className={s.headingInner} lang={mainLang}>
                     {children}
                 </span>
