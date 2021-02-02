131d5d1c93fa587cc54523ceb1421633fae698ab
Timur Çelikel <me@timurcelikel.com>
2021-02-02 10:17:57 +0100

    TranslationList: add link to add translation

--- src/TranslationsList/index.tsx
+++ src/TranslationsList/index.tsx
@@ -3,10 +3,12 @@ import { useTranslations } from '../dataHooks';
 import { Trans, useTranslation } from 'react-i18next';
 import { TermWithLang } from '../TermWithLang';
 import { Term } from '../types';
+import { Link } from 'react-router-dom';
 
 export function TranslationsList({ term }: { term: Term }) {
     const [translations] = useTranslations(term.id);
     const { t } = useTranslation();
+    const addTermLink = `/term/${term.id}/translation/add`;
 
     if (translations === undefined) {
         return <>{t('common.loading')}</>;
@@ -21,15 +23,19 @@ export function TranslationsList({ term }: { term: Term }) {
                     values={{ term: term.value }}
                     components={{ TermWithLang: <TermWithLang lang={term.lang}>foo</TermWithLang> }}
                 />
+                <Link to={addTermLink}>{t('common.entities.translation.add')}</Link>
             </div>
         );
     }
 
     return (
-        <ul className={s.list}>
-            {translations.map(translation => (
-                <li key={translation.id}>{translation.value}</li>
-            ))}
-        </ul>
+        <div>
+            <ul className={s.list}>
+                {translations.map(translation => (
+                    <li key={translation.id}>{translation.value}</li>
+                ))}
+            </ul>
+            <Link to={addTermLink}>{t('common.entities.translation.add')}</Link>
+        </div>
     );
 }
