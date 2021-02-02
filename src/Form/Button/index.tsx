5ef369f582fb0c9c771de6408f789f1795585e5f
Timur Çelikel <me@timurcelikel.com>
2021-02-02 10:29:57 +0100

    Button: simplify a little

--- src/Form/Button/index.tsx
+++ src/Form/Button/index.tsx
@@ -2,29 +2,19 @@ import { Link, LinkProps } from 'react-router-dom';
 import s from './style.module.css';
 
 interface Props extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-    children: React.ReactNode;
     primary?: boolean;
 }
 
-export default function Button({ children, primary = false, ...props }: Props) {
-    return (
-        <button className={primary ? s.buttonPrimary : s.button} {...props}>
-            {children}
-        </button>
-    );
+export default function Button({ primary = false, ...props }: Props) {
+    return <button className={primary ? s.buttonPrimary : s.button} {...props} />;
 }
 
 interface ButtonLinkProps extends LinkProps {
-    children: React.ReactNode;
     primary?: boolean;
 }
 
-export function ButtonLink({ children, primary = false, ...props }: ButtonLinkProps) {
-    return (
-        <Link className={primary ? s.buttonPrimary : s.button} {...props}>
-            {children}
-        </Link>
-    );
+export function ButtonLink({ primary = false, ...props }: ButtonLinkProps) {
+    return <Link className={primary ? s.buttonPrimary : s.button} {...props} />;
 }
 
 type ButtonContainerProps = {
