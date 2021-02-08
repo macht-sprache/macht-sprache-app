c601568a1914da386f5155d566def7e4aa15cd44
Kolja Lange <mail@klange.org>
2021-02-08 12:36:24 +0100

    Functions: set up emulation, use service account

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/functions';
 
 const appConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,6 +15,7 @@ const appConfig = {
 const app = firebase.initializeApp(appConfig);
 const auth = app.auth();
 const db = app.firestore();
+const functions = app.functions();
 
 if (process.env.REACT_APP_AUTH_EMULATOR_PORT) {
     auth.useEmulator(window.location.origin.replace(window.location.port, process.env.REACT_APP_AUTH_EMULATOR_PORT));
@@ -23,4 +25,8 @@ if (process.env.REACT_APP_FIRESTORE_EMULATOR_PORT) {
     db.useEmulator(window.location.hostname, parseInt(process.env.REACT_APP_FIRESTORE_EMULATOR_PORT));
 }
 
-export { app, auth, db };
+if (process.env.REACT_APP_FUNCTIONS_EMULATOR_PORT) {
+    functions.useEmulator(window.location.hostname, parseInt(process.env.REACT_APP_FUNCTIONS_EMULATOR_PORT));
+}
+
+export { app, auth, db, functions };
