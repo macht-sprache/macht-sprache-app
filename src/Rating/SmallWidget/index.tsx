3adcf88d19eaa51ad8968e2d7d27cec63481b694
Timur Çelikel <me@timurcelikel.com>
2021-02-17 12:49:36 +0100

    SmallRatingWidget: add user rating dot

--- src/Rating/SmallWidget/index.tsx
+++ src/Rating/SmallWidget/index.tsx
@@ -9,6 +9,7 @@ type SmallRatingWidgetProps = {
 
 export function SmallRatingWidget({ ratings, userRating, lang }: SmallRatingWidgetProps) {
     const max = Math.max(...ratings);
+    const columnWidth = 1 / ratings.length;
 
     return (
         <div className={s.container} lang={lang}>
@@ -17,6 +18,12 @@ export function SmallRatingWidget({ ratings, userRating, lang }: SmallRatingWidg
                     <div key={index} style={{ height: `${(rating / max) * 100}%` }} className={s.rating} />
                 ))}
             </div>
+            {userRating && (
+                <div
+                    className={s.userRating}
+                    style={{ left: `${(userRating.rating * (1 - columnWidth) + columnWidth / 2) * 100}%` }}
+                />
+            )}
         </div>
     );
 }
