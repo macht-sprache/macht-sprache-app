a1051ea6630f151d51cb226056c53d8be6e62e6d
Timur Çelikel <me@timurcelikel.com>
2021-02-25 11:00:40 +0100

    TermWidthLang: add wrapper that does not need a proper Term or Translation

--- src/TermWithLang/index.tsx
+++ src/TermWithLang/index.tsx
@@ -1,4 +1,4 @@
-import { Term, Translation } from '../types';
+import { Lang, Term, Translation } from '../types';
 import s from './style.module.css';
 
 type TermWithLangProps = {
@@ -6,9 +6,18 @@ type TermWithLangProps = {
 };
 
 export function TermWithLang({ term }: TermWithLangProps) {
+    return <WrappedInLangColor lang={term.lang}>{term.value}</WrappedInLangColor>;
+}
+
+type WrappedInLangColorProps = {
+    lang: Lang;
+    children?: React.ReactNode;
+};
+
+export function WrappedInLangColor({ lang, children }: WrappedInLangColorProps) {
     return (
-        <span className={s.term} lang={term.lang}>
-            {term.value}
+        <span className={s.term} lang={lang}>
+            {children}
         </span>
     );
 }
