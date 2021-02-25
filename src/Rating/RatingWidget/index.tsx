fb6640cd9d3ee6aea31a3264b6ab3c967b436a5a
Timur Çelikel <me@timurcelikel.com>
2021-02-25 17:44:32 +0100

    RatingWidget: add translation

--- src/Rating/RatingWidget/index.tsx
+++ src/Rating/RatingWidget/index.tsx
@@ -157,7 +157,7 @@ function RatingDisplay({
                         )}
                     </div>
                 ))}
-                {sumOfAllRatings === 0 && <div className={s.emtpyMessage}>No data yet.</div>}
+                {sumOfAllRatings === 0 && <div className={s.emtpyMessage}>{t('rating.noData')}</div>}
             </div>
             {size === 'large' && (
                 <div aria-hidden="true" className={s.permanentSliderLabel}>
