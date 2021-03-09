7d66c817494ed18c2dc92bac8d15f2fbd1e7e120
Timur Çelikel <me@timurcelikel.com>
2021-03-09 16:44:20 +0100

    HomePage: add news and intro

--- src/HomePage/index.tsx
+++ src/HomePage/index.tsx
@@ -1,10 +1,31 @@
+import { useTranslation } from 'react-i18next';
 import Header from '../Header';
+import { ColumnHeading, Columns } from '../Layout/Columns';
+import { NewsFeed } from '../NewsFeed';
+import { useWpPage } from '../useWpHooks';
+
+const ABOUT_SLUGS = {
+    en: 'about-macht-sprache-short-version-landing-page',
+    de: 'ueber-macht-sprache-kurzversion-startseite',
+};
 
 export default function Home() {
+    const { t } = useTranslation();
+    const { response } = useWpPage(ABOUT_SLUGS);
+
     return (
         <>
-            <Header>Home</Header>
-            <p>Your're logged in or we have launched :-) </p>
+            <Header>macht.sprache.</Header>
+            <Columns>
+                <div>
+                    <ColumnHeading>{t('home.about')}</ColumnHeading>
+                    <div dangerouslySetInnerHTML={{ __html: response ? response.body : t('common.loading') }} />
+                </div>
+                <div>
+                    <ColumnHeading>{t('home.news')}</ColumnHeading>
+                    <NewsFeed />
+                </div>
+            </Columns>
         </>
     );
 }
