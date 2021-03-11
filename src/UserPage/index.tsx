e2bceef18f5589976df92b0638ecb33e8343b4e0
Timur Çelikel <me@timurcelikel.com>
2021-03-11 15:39:40 +0100

    UserPage: add text & translations, invert checkbox

--- src/UserPage/index.tsx
+++ src/UserPage/index.tsx
@@ -1,12 +1,15 @@
 import { useCallback } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
 import Header from '../Header';
 import { useUser, useUserSettings } from '../hooks/appContext';
 import { collections } from '../hooks/data';
+import { ColumnHeading, Columns, SingleColumn } from '../Layout/Columns';
 import { UserSettings } from '../types';
 
 export default function UserPage() {
     const user = useUser()!;
     const userSettings = useUserSettings()!;
+    const { t } = useTranslation();
 
     const onChange = useCallback(
         (update: Partial<UserSettings>) => collections.userSettings.doc(user.id).set({ ...userSettings, ...update }),
@@ -15,15 +18,22 @@ export default function UserPage() {
 
     return (
         <>
-            <Header>Settings</Header>
-            <label>
-                <input
-                    type="checkbox"
-                    checked={userSettings.showRedacted}
-                    onChange={event => onChange({ showRedacted: event.target.checked })}
-                />{' '}
-                Show Redacted
-            </label>
+            <Header topHeading={[{ inner: t('userPage.title') }]}>{user.displayName}</Header>
+            <SingleColumn>
+                <ColumnHeading>{t('common.settings')}</ColumnHeading>
+
+                <p>
+                    <Trans t={t} i18nKey="userPage.hideRedactedDescription" />
+                </p>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={!userSettings.showRedacted}
+                        onChange={event => onChange({ showRedacted: !event.target.checked })}
+                    />{' '}
+                    {t('userPage.hideRedacted')}
+                </label>
+            </SingleColumn>
         </>
     );
 }
