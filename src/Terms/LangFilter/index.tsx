ead39847b364fb28727810bcb0351e3bed61601b
Timur Çelikel <me@timurcelikel.com>
2021-03-17 11:14:25 +0100

    Terms: refactor out LangFilter

---
+++ src/Terms/LangFilter/index.tsx
@@ -0,0 +1,49 @@
+import { useTranslation } from 'react-i18next';
+import { HorizontalRadio, HorizontalRadioContainer } from '../../Form/HorizontalRadio';
+import { langA, langB } from '../../languages';
+import { Lang } from '../../types';
+
+type LangFilterProps = {
+    setLangFilter: (lang: Lang | undefined) => void;
+    langFilter?: Lang;
+};
+
+export function LangFilter({ setLangFilter, langFilter }: LangFilterProps) {
+    const { t } = useTranslation();
+
+    const langFilters: { value?: Lang; label: string; longLabel: string }[] = [
+        {
+            label: t('nav.filter.all.label'),
+            longLabel: t('nav.filter.all.longLabel'),
+        },
+        {
+            value: langB,
+            label: langB.toUpperCase(),
+            longLabel: t(`nav.filter.${langB}.longLabel` as const),
+        },
+        {
+            value: langA,
+            label: langA.toUpperCase(),
+            longLabel: t(`nav.filter.${langA}.longLabel` as const),
+        },
+    ];
+
+    return (
+        <HorizontalRadioContainer>
+            {langFilters.map(({ value, label, longLabel }) => (
+                <HorizontalRadio
+                    key={value ?? ''}
+                    value={value ?? ''}
+                    label={label}
+                    name="language_nav_main"
+                    checked={value === langFilter}
+                    aria-label={longLabel}
+                    onChange={() => {
+                        setLangFilter(value);
+                    }}
+                    background={value || 'striped'}
+                />
+            ))}
+        </HorizontalRadioContainer>
+    );
+}
