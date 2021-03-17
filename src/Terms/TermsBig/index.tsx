6130e71d99537834ba5f53fea2ae3f594ba47a25
Timur Çelikel <me@timurcelikel.com>
2021-03-17 11:15:31 +0100

    TermsPage: make terms bigger, add translations

---
+++ src/Terms/TermsBig/index.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { generatePath, NavLink } from 'react-router-dom';
+import { useTerms, useTranslations } from '../../hooks/data';
+import { Redact } from '../../RedactSensitiveTerms';
+import { TERM } from '../../routes';
+import { TermWithLang } from '../../TermWithLang';
+import { Lang, Term } from '../../types';
+import { useLang } from '../../useLang';
+import { LangFilter } from '../LangFilter';
+import s from './style.module.css';
+
+export function TermsBig() {
+    const terms = useTerms();
+    const [langFilter, setLangFilter] = useState<Lang>();
+    const [lang] = useLang();
+    const sortedTerms = terms
+        .filter(term => !langFilter || langFilter === term.lang)
+        .sort(({ value: valueA }, { value: valueB }) => valueA.localeCompare(valueB, lang));
+
+    return (
+        <>
+            <LangFilter langFilter={langFilter} setLangFilter={setLangFilter} />
+            <div>
+                <ul className={s.terms}>
+                    {sortedTerms.map(term => (
+                        <TermItem key={term.id} term={term} />
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
+}
+
+function TermItem({ term }: { term: Term }) {
+    const translations = useTranslations(term.id);
+
+    console.log(translations);
+
+    return (
+        <li className={s.term}>
+            <NavLink
+                to={generatePath(TERM, { termId: term.id })}
+                className={s.termLink}
+                activeClassName={s.termLinkActive}
+                lang={term.lang}
+            >
+                <div className={s.termValue}>
+                    <Redact>{term.value}</Redact>
+                </div>
+                <ul className={s.translations}>
+                    {translations.map(term => (
+                        <li key={term.id} className={s.translation}>
+                            <TermWithLang term={term} />
+                        </li>
+                    ))}
+                </ul>
+            </NavLink>
+        </li>
+    );
+}
--- src/TermsPage/index.tsx
+++ src/TermsPage/index.tsx
@@ -1,15 +1,28 @@
 import { useTranslation } from 'react-i18next';
+import { ButtonContainer, ButtonLink } from '../Form/Button';
 import Header from '../Header';
 import { SingleColumn } from '../Layout/Columns';
-import { Terms } from '../Terms';
+import { LoginHint } from '../LoginHint';
+import { TERM_ADD } from '../routes';
+import { TermsBig } from '../Terms/TermsBig';
 
 export function TermsPage() {
     const { t } = useTranslation();
     return (
         <>
-            <Header>{t('common.entities.term.value_plural')}</Header>
+            <Header
+                subLine={
+                    <LoginHint i18nKey="term.registerToAdd">
+                        <ButtonContainer align="left">
+                            <ButtonLink to={TERM_ADD}>{t('common.entities.term.add')}</ButtonLink>
+                        </ButtonContainer>
+                    </LoginHint>
+                }
+            >
+                {t('common.entities.term.value_plural')}
+            </Header>
             <SingleColumn>
-                <Terms />
+                <TermsBig />
             </SingleColumn>
         </>
     );
