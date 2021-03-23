9f10fd9ae39d44180cfb0f020d3a444d8a663aa4
Timur Çelikel <me@timurcelikel.com>
2021-03-23 13:48:33 +0100

    News: styling

--- src/NewsFeed/index.tsx
+++ src/NewsFeed/index.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import { ButtonAnchor } from '../Form/Button';
 import { FormatDate } from '../FormatDate';
 import { useWpPosts, WpImage } from '../useWpHooks';
 import s from './style.module.css';
@@ -13,20 +14,32 @@ export function NewsFeed() {
         <div>
             {isLoading && <>{t('common.loading')}</>}
             {response?.map(({ title, link, excerpt, date, featuredMedia }, index) => (
-                <article key={index} className={s.article}>
-                    <a href={link} target="_blank" rel="noreferrer" className={s.link}>
-                        <h1 className={s.heading} dangerouslySetInnerHTML={{ __html: title }} />
-                        <div className={s.body}>
-                            {featuredMedia && <WpImage sizes="300px" className={s.image} image={featuredMedia} />}
-                            <div>
-                                <div className={s.date}>
-                                    <FormatDate date={date} />
-                                </div>
-                                <div className={s.excerpt} dangerouslySetInnerHTML={{ __html: excerpt }} />
-                                <div className={s.clickForMore}>{t('newsFeed.clickForMore')}</div>
-                            </div>
+                <article
+                    key={index}
+                    className={s.article}
+                    onClick={() => {
+                        window.open(link);
+                    }}
+                >
+                    <h1 className={s.heading}>
+                        <a
+                            href={link}
+                            target="_blank"
+                            rel="noreferrer"
+                            className={s.link}
+                            dangerouslySetInnerHTML={{ __html: title }}
+                        />
+                    </h1>
+                    {featuredMedia && <WpImage sizes="600px" className={s.image} image={featuredMedia} />}
+                    <div className={s.body}>
+                        <div className={s.date}>
+                            <FormatDate date={date} />
                         </div>
-                    </a>
+                        <div className={s.excerpt} dangerouslySetInnerHTML={{ __html: excerpt }} />
+                        <ButtonAnchor href={link} target="_blank" rel="noreferrer" className={s.clickForMore}>
+                            {t('newsFeed.clickForMore')}
+                        </ButtonAnchor>
+                    </div>
                 </article>
             ))}
         </div>
