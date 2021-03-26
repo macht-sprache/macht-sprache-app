f50499853111fd1528e747c01eaf7aa64d734e55
Timur Çelikel <me@timurcelikel.com>
2021-03-26 15:11:00 +0100

    ModalDialog: add isDismissable and width option

---
+++ src/ContentWarning/index.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import Button, { ButtonContainer } from '../Form/Button';
+import { useUser } from '../hooks/appContext';
+import { Columns } from '../Layout/Columns';
+import { ModalDialog } from '../ModalDialog';
+import s from './style.module.css';
+import Illustration from './illustration.jpg';
+
+const LOCAL_STORAGE_ID = 'overlayShown';
+
+export function ContentWarning() {
+    const user = useUser();
+    const { t } = useTranslation();
+    const [showOverlay, setShowOverlay] = useState(!(localStorage.getItem(LOCAL_STORAGE_ID) === 'true') && !user);
+    console.log(user, showOverlay);
+
+    function dismissOverlay() {
+        setShowOverlay(false);
+        localStorage.setItem(LOCAL_STORAGE_ID, 'true');
+    }
+
+    if (!showOverlay) return null;
+
+    return (
+        <ModalDialog
+            isDismissable={false}
+            onClose={() => {
+                dismissOverlay();
+            }}
+            title={t('contentWarning.title')}
+            width="wide"
+        >
+            <div className={s.body}>
+                <img className={s.illustration} src={Illustration} alt="" />
+                <div>
+                    <p className={s.warning}>{t('contentWarning.warning')}</p>
+
+                    <ButtonContainer>
+                        <Button onClick={dismissOverlay}>{t('contentWarning.button')}</Button>
+                    </ButtonContainer>
+                </div>
+            </div>
+        </ModalDialog>
+    );
+}
--- src/ModalDialog/index.tsx
+++ src/ModalDialog/index.tsx
@@ -4,17 +4,20 @@ import { useOverlay, usePreventScroll, useModal, OverlayContainer } from '@react
 import { useDialog } from '@react-aria/dialog';
 import { FocusScope } from '@react-aria/focus';
 import { useRef } from 'react';
+import clsx from 'clsx';
 
 type ModalDialogProps = {
     onClose: () => void;
     title: React.ReactNode;
     children: React.ReactNode;
+    isDismissable?: boolean;
+    width?: 'medium' | 'wide';
 };
 
-export function ModalDialog({ title, children, onClose }: ModalDialogProps) {
+export function ModalDialog({ title, children, onClose, isDismissable = true, width = 'medium' }: ModalDialogProps) {
     usePreventScroll();
     const ref = useRef<HTMLDivElement>(null);
-    const { overlayProps } = useOverlay({ onClose, isOpen: true, isDismissable: true }, ref);
+    const { overlayProps } = useOverlay({ onClose, isOpen: true, isDismissable }, ref);
     const { modalProps } = useModal();
     const { dialogProps, titleProps } = useDialog({}, ref);
 
@@ -22,7 +25,13 @@ export function ModalDialog({ title, children, onClose }: ModalDialogProps) {
         <OverlayContainer>
             <div className={s.background}>
                 <FocusScope contain restoreFocus autoFocus>
-                    <div {...overlayProps} {...dialogProps} {...modalProps} ref={ref} className={s.overlay}>
+                    <div
+                        {...overlayProps}
+                        {...dialogProps}
+                        {...modalProps}
+                        ref={ref}
+                        className={clsx(s[width], s.overlay)}
+                    >
                         <h3 {...titleProps} className={s.title}>
                             {title}
                         </h3>
