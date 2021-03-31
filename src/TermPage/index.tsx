1d0b491c0b259498a993cdf173f4e73f0fb17a71
Timur Çelikel <me@timurcelikel.com>
2021-03-31 12:12:33 +0200

    Terms: add edit

--- src/TermPage/index.tsx
+++ src/TermPage/index.tsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import Comments from '../Comments';
+import Button, { ButtonContainer } from '../Form/Button';
+import { Input, Select } from '../Form/Input';
+import InputContainer from '../Form/InputContainer';
 import { FormatDate } from '../FormatDate';
 import Header from '../Header';
+import { useAppContext } from '../hooks/appContext';
 import { collections, useTerm } from '../hooks/data';
+import { langA, langB } from '../languages';
 import { FullWidthColumn, SingleColumn } from '../Layout/Columns';
+import LinkButton from '../LinkButton';
+import { ModalDialog } from '../ModalDialog';
 import { Redact, useRedacted } from '../RedactSensitiveTerms';
 import { TermWithLang } from '../TermWithLang';
 import { TranslationsList } from '../TranslationsList';
+import { Lang, Term } from '../types';
 import { getDominantLanguageClass } from '../useLangCssVars';
 
 export default function TermPage() {
@@ -21,14 +30,17 @@ export default function TermPage() {
             <Header
                 capitalize
                 subLine={
-                    <Trans
-                        t={t}
-                        i18nKey="common.addedOn"
-                        components={{
-                            User: term.creator.displayName,
-                            FormatDate: <FormatDate date={term.createdAt} />,
-                        }}
-                    />
+                    <>
+                        <Trans
+                            t={t}
+                            i18nKey="common.addedOn"
+                            components={{
+                                User: term.creator.displayName,
+                                FormatDate: <FormatDate date={term.createdAt} />,
+                            }}
+                        />
+                        <EditTerm term={term} />
+                    </>
                 }
                 mainLang={term.lang}
             >
@@ -55,5 +67,88 @@ export default function TermPage() {
                 </div>
             </SingleColumn>
         </>
+    );
+}
+
+function EditTerm({ term }: { term: Term }) {
+    const { user, userProperties } = useAppContext();
+    const [editOpen, setEditOpen] = useState(false);
+    const { t } = useTranslation();
+    const canEdit = term.creator.id === user?.id || userProperties?.admin;
+
+    if (!canEdit) {
+        return null;
+    }
+
+    return (
+        <div>
+            <LinkButton
+                onClick={() => {
+                    setEditOpen(true);
+                }}
+            >
+                {t('common.entities.term.edit')}
+            </LinkButton>
+            {editOpen && (
+                <EditTermOverlay
+                    term={term}
+                    onClose={() => {
+                        setEditOpen(false);
+                    }}
+                />
+            )}
+        </div>
+    );
+}
+
+function EditTermOverlay({ term, onClose }: { term: Term; onClose: () => void }) {
+    const { t } = useTranslation();
+    const [saving, setIsSaving] = useState(false);
+    const [value, setValue] = useState(term.value);
+    const [lang, setLang] = useState<Lang>(term.lang);
+
+    const onSave = () => {
+        setIsSaving(true);
+        collections.terms
+            .doc(term.id)
+            .set({ ...term, value, lang })
+            .then(() => {
+                setIsSaving(false);
+                onClose();
+            });
+    };
+
+    return (
+        <ModalDialog title={t('common.entities.term.edit')} onClose={onClose}>
+            {saving ? (
+                <>{t('common.saving')}</>
+            ) : (
+                <>
+                    <InputContainer>
+                        <Input
+                            label={t('common.entities.term.value')}
+                            span={3}
+                            value={value}
+                            onChange={({ target: { value } }) => setValue(value)}
+                        />
+                        <Select
+                            label={t('common.langLabels.language')}
+                            span={1}
+                            value={lang}
+                            onChange={({ target: { value } }) => setLang(value as Lang)}
+                        >
+                            <option value={langA}>{t(`common.langLabels.${langA}` as const)}</option>
+                            <option value={langB}>{t(`common.langLabels.${langB}` as const)}</option>
+                        </Select>
+                    </InputContainer>
+                    <ButtonContainer>
+                        <Button onClick={onClose}>{t('common.formNav.cancel')}</Button>
+                        <Button onClick={onSave} primary>
+                            {t('common.formNav.save')}
+                        </Button>
+                    </ButtonContainer>
+                </>
+            )}
+        </ModalDialog>
     );
 }
