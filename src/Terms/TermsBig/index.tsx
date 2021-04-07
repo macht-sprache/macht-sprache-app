45001d1633fb398127b03b81a17de8b97db03b53
Timur Çelikel <me@timurcelikel.com>
2021-04-07 18:22:47 +0200

    TermsPage: add search

--- src/Terms/TermsBig/index.tsx
+++ src/Terms/TermsBig/index.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ButtonLink } from '../../Form/Button';
+import { InlineInput } from '../../Form/InlineInput';
 import { useUser } from '../../hooks/appContext';
 import { GetList } from '../../hooks/fetch';
-import { LoginHint } from '../../LoginHint';
 import { TERM_ADD } from '../../routes';
 import { Lang, Term } from '../../types';
 import { useLang } from '../../useLang';
@@ -17,6 +17,7 @@ type Props = {
 
 export function TermsBig({ getTerms }: Props) {
     const [langFilter, setLangFilter] = useState<Lang>();
+    const [textFilter, setTextFilter] = useState('');
     const [lang] = useLang();
     const { t } = useTranslation();
     const user = useUser();
@@ -24,12 +25,21 @@ export function TermsBig({ getTerms }: Props) {
     const terms = getTerms();
     const sortedTerms = terms
         .filter(term => !langFilter || langFilter === term.lang)
+        .filter(term => !textFilter || term.value.toLowerCase().includes(textFilter.toLowerCase()))
         .sort(({ value: valueA }, { value: valueB }) => valueA.localeCompare(valueB, lang));
 
     return (
         <>
             <div className={s.filters}>
                 <LangFilter langFilter={langFilter} setLangFilter={setLangFilter} />
+                <InlineInput
+                    label={t('term.search')}
+                    placeholder={t('term.search')}
+                    type="text"
+                    value={textFilter}
+                    onChange={({ target: { value } }) => setTextFilter(value)}
+                    onCancel={() => setTextFilter('')}
+                />
 
                 {user && (
                     <ButtonLink size="small" to={TERM_ADD}>
