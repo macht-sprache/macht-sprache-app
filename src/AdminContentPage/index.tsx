9d037ae76f8f5b99299df2f93308f403711002dc
Timur Çelikel <me@timurcelikel.com>
2021-04-08 13:46:29 +0200

    CommentList: add version with links

--- src/AdminContentPage/index.tsx
+++ src/AdminContentPage/index.tsx
@@ -1,11 +1,9 @@
-import { generatePath, Link } from 'react-router-dom';
-import { CommentItem } from '../Comments/CommentItem';
+import { CommentListWithLinks } from '../Comments/CommentList';
 import Header from '../Header';
 import { collections } from '../hooks/data';
-import { GetList, useCollection, useDocument } from '../hooks/fetch';
+import { GetList, useCollection } from '../hooks/fetch';
 import { ColumnHeading, SingleColumn } from '../Layout/Columns';
-import { TERM } from '../routes';
-import { Comment, DocReference, Term, Translation, TranslationExample } from '../types';
+import { Comment } from '../types';
 
 export default function AdminContentPage() {
     const getComments = useCollection(collections.comments.orderBy('createdAt', 'desc').limit(100));
@@ -26,47 +24,8 @@ function Comments({ getComments }: { getComments: GetList<Comment> }) {
     return (
         <div>
             {comments.map(comment => {
-                return <CommentWithLink key={comment.id} comment={comment} />;
+                return <CommentListWithLinks key={comment.id} comments={comments} />;
             })}
         </div>
     );
-}
-
-function CommentWithLink({ comment }: { comment: Comment }) {
-    const getDocument = useDocument(comment.ref);
-    const linkedDocument = getDocument();
-
-    return (
-        <div style={{ marginBottom: '2rem' }}>
-            <LinkToDocument document={linkedDocument} commentRef={comment.ref} />
-            <CommentItem key={comment.id} comment={comment} />
-        </div>
-    );
-}
-
-function LinkToDocument({
-    document,
-    commentRef,
-}: {
-    document: Term | Translation | TranslationExample;
-    commentRef: DocReference<Term | Translation | TranslationExample>;
-}) {
-    if (commentRef.parent.id === 'terms') {
-        const term = document as Term;
-
-        return (
-            <div style={{ marginBottom: '.5rem' }}>
-                On <Link to={generatePath(TERM, { termId: commentRef.id })}>{term.value}</Link>
-            </div>
-        );
-    }
-
-    if (commentRef.parent.id === 'translationExamples') {
-        const example = document as TranslationExample;
-        console.log(example);
-
-        return <div style={{ marginBottom: '.5rem' }}>TODO: NEEDS LINK</div>;
-    }
-
-    return null;
 }
--- src/Comments/CommentList/index.tsx
+++ src/Comments/CommentList/index.tsx
@@ -1,4 +1,7 @@
-import { Comment } from '../../types';
+import { generatePath, Link } from 'react-router-dom';
+import { useDocument } from '../../hooks/fetch';
+import { TERM, TRANSLATION_EXAMPLE_REDIRECT } from '../../routes';
+import { Comment, DocReference, Source, Term, Translation, TranslationExample } from '../../types';
 import { CommentItem } from '../CommentItem';
 import s from './style.module.css';
 
@@ -13,5 +16,68 @@ export function CommentList({ comments }: CommentListProps) {
                 return <CommentItem key={comment.id} comment={comment} />;
             })}
         </div>
+    );
+}
+
+export function CommentListWithLinks({ comments }: CommentListProps) {
+    return (
+        <div className={s.container}>
+            {comments.map(comment => {
+                return <CommentWithLink key={comment.id} comment={comment} />;
+            })}
+        </div>
+    );
+}
+
+function CommentWithLink({ comment }: { comment: Comment }) {
+    const getDocument = useDocument(comment.ref);
+    const linkedDocument = getDocument();
+
+    return (
+        <div style={{ marginBottom: '2rem' }}>
+            <LinkToDocument document={linkedDocument} documentRef={comment.ref} />
+            <CommentItem key={comment.id} comment={comment} />
+        </div>
+    );
+}
+
+function LinkToDocument({
+    document,
+    documentRef,
+}: {
+    document: Term | Translation | TranslationExample;
+    documentRef: DocReference<Term | Translation | TranslationExample>;
+}) {
+    if (documentRef.parent.id === 'terms') {
+        const term = document as Term;
+
+        return (
+            <div style={{ marginBottom: '.5rem' }}>
+                On <Link to={generatePath(TERM, { termId: documentRef.id })}>{term.value}</Link>
+            </div>
+        );
+    }
+
+    if (documentRef.parent.id === 'translationExamples') {
+        const example = document as TranslationExample;
+
+        return (
+            <div style={{ marginBottom: '.5rem' }}>
+                On <LinkToTranslationExample example={example} exampleId={documentRef.id} />
+            </div>
+        );
+    }
+
+    return null;
+}
+
+function LinkToTranslationExample({ example, exampleId }: { example: TranslationExample; exampleId: string }) {
+    const getOriginalSource = useDocument(example.original.source as DocReference<Source>);
+    const original = getOriginalSource();
+
+    return (
+        <Link to={generatePath(TRANSLATION_EXAMPLE_REDIRECT, { translationExampleId: exampleId })}>
+            {original.title}
+        </Link>
     );
 }
