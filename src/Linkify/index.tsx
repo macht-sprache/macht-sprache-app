afb86d26da3794cbff45c1d4bdba587a1b9e1e76
Kolja Lange <mail@klange.org>
2021-04-17 16:06:30 +0200

    Linkify: don't open internal links in new tab

--- src/Linkify/index.tsx
+++ src/Linkify/index.tsx
@@ -1,5 +1,6 @@
 import LinkifyIt from 'linkify-it';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import { removeHttpsWwwPageParams, trimString } from '../utils';
 import s from './style.module.css';
 
@@ -12,12 +13,7 @@ type Props = {
 function Linkify({ children }: Props) {
     const parts = (linkify.match(children) || []).reduce<React.ReactNode[]>((acc, cur, index, array) => {
         const prevIndex = array[index - 1]?.lastIndex || 0;
-        acc.push(
-            children.substring(prevIndex, cur.index),
-            <a key={index} target="_blank" href={cur.url} rel="noopener noreferrer" className={s.link}>
-                {trimString(removeHttpsWwwPageParams(cur.url))}
-            </a>
-        );
+        acc.push(children.substring(prevIndex, cur.index), <LinkWrapper key={index} url={cur.url} />);
 
         if (index === array.length - 1) {
             acc.push(children.substring(cur.lastIndex));
@@ -27,5 +23,36 @@ function Linkify({ children }: Props) {
     }, []);
     return <>{parts.length ? parts : children}</>;
 }
+
+function LinkWrapper(props: { url: string }) {
+    const url = useMemo(() => {
+        try {
+            return new URL(props.url);
+        } catch {
+            return null;
+        }
+    }, [props.url]);
+    const linkText = trimString(removeHttpsWwwPageParams(props.url));
+
+    if (url?.origin === window.location.origin) {
+        return (
+            <InternalLink url={url} className={s.link}>
+                {linkText}
+            </InternalLink>
+        );
+    } else {
+        return (
+            <a target="_blank" href={props.url} rel="noopener noreferrer" className={s.link}>
+                {linkText}
+            </a>
+        );
+    }
+}
+
+const InternalLink: React.FC<{ url: URL; className: string }> = ({ url, className, children }) => (
+    <Link to={url.toString().replace(url.origin, '')} className={className}>
+        {children}
+    </Link>
+);
 
 export default memo(Linkify);
