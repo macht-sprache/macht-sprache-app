3c93f833c912cd5f363cb7f68e33bbd53dac3a7f
Timur Çelikel <me@timurcelikel.com>
2021-04-19 15:06:11 +0200

    CommentWrapper: remove superfluous componente

--- src/Comments/CommentWrapper/index.tsx
+++
@@ -1,9 +0,0 @@
-import s from './style.module.css';
-
-type CommentsProps = {
-    children: React.ReactNode;
-};
-
-export function CommentWrapper({ children }: CommentsProps) {
-    return <div className={s.container}>{children}</div>;
-}
