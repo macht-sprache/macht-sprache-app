b260b75df1cd548d82f351b92c792960a1e7d692
Timur Çelikel <me@timurcelikel.com>
2021-09-08 18:10:55 +0200

    TranslationHelper: overlay, dummy content

--- src/TranslationHelperPage/index.tsx
+++ src/TranslationHelperPage/index.tsx
@@ -9,9 +9,39 @@ import { langA, langB } from '../languages';
 import { SingleColumn } from '../Layout/Columns';
 import { Lang } from '../types';
 import s from './style.module.css';
+import clsx from 'clsx';
 
 type TextType = 'original' | 'translation';
 
+type TranslationList = {
+    word: string;
+    definition: string;
+    link: string;
+    translations: { word: string; definition: string }[];
+};
+
+const dummyTranslationRace: TranslationList = {
+    word: 'Race',
+    definition: 'In references to human beings, race is an identity category.',
+    link: 'https://www.machtsprache.de/term/b2LXNYVQJ3BH5JFDrB8m',
+    translations: [
+        {
+            word: 'Race',
+            definition:
+                'Der Englische Begriff Race kann im Deutschen beibehalten werden. Er beschreibt ein Identitätskonzept und kann als solches als feststehender Ausdruck verstanden werden.',
+        },
+        {
+            word: 'Rassifizierung',
+            definition:
+                'Rassifizierung ist keine direkte Übersetzung für Race. Der Begriff beschreibt den gesellschaftlichen Prozess, der Menschen einer Race zuordnet, also einer bestimmten Identität.',
+        },
+        {
+            word: 'Race',
+            definition:
+                'Rasse ist die wohl bekannteste Übersetzungsoption für Race. Aber die Bedeutung der Begriffe hat historisch unterschiedliche Entwicklungen durchgemacht. Rasse ist beispielsweise eng verbunden mit kolonialer, pseudowissenschaftlicher Rassenforschung und der Nazizeit. Bei Race ist klarer, dass es sich um ein Konstrukt handelt. ',
+        },
+    ],
+};
 export default function TranslationHelperPage() {
     const [isSubmitted, setIsSubmitted] = useState(false);
 
@@ -43,9 +73,13 @@ function Analysis({ onCancel }: { onCancel: () => void }) {
             <p>Hover over the words for more information.</p>
             <div className={s.analysisContainer}>
                 <p>
-                    I conceptualize this process as <SensibleWord>white fragility</SensibleWord>. Though{' '}
-                    <SensibleWord>white fragility</SensibleWord> is triggered by discomfort and anxiety, it is born of
-                    superiority and entitlement. <SensibleWord>White fragility</SensibleWord>
+                    Led to factors like <SensibleWord translationList={dummyTranslationRace}>race</SensibleWord>,
+                    development and location - to who does and who doesn’t make a ‚relatable’ victim of terror.
+                    <br />I conceptualize this process as{' '}
+                    <SensibleWord translationList={dummyTranslationRace}>white fragility</SensibleWord>. Though{' '}
+                    <SensibleWord translationList={dummyTranslationRace}>white fragility</SensibleWord> is triggered by
+                    discomfort and anxiety, it is born of superiority and entitlement.{' '}
+                    <SensibleWord translationList={dummyTranslationRace}>White fragility</SensibleWord>
                     is not weakness per se. In fact, it is a powerful means of white racial control and the protection
                     of white advantage.
                 </p>
@@ -59,10 +93,35 @@ function Analysis({ onCancel }: { onCancel: () => void }) {
     );
 }
 
-function SensibleWord({ children }: { children: React.ReactNode }) {
+function SensibleWord({ children, translationList }: { children: React.ReactNode; translationList: TranslationList }) {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const tooltip = (
+        <div className={s.overlay}>
+            <span className={s.overlayWord}>{translationList.word}</span>: {translationList.definition}
+            <h4 className={s.overlayHeading}>Possible translations</h4>
+            <ul className={s.overlayTranslationList}>
+                {translationList.translations.map((translation, index) => (
+                    <li key={index} className={s.overlayTranslation}>
+                        <span className={s.overlayWord}>{translation.word}</span>: {translation.definition}
+                    </li>
+                ))}
+            </ul>
+            <a href={translationList.link}>More information</a>
+        </div>
+    );
+
     return (
-        <Tooltip overlay={<>huhu</>} placement="bottom">
-            <span className={s.sensibleWord}>{children}</span>
+        <Tooltip
+            overlay={tooltip}
+            placement="bottom"
+            onVisibleChange={visible => {
+                setIsVisible(visible);
+            }}
+            mouseEnterDelay={0.2}
+            mouseLeaveDelay={0.2}
+        >
+            <span className={clsx(s.sensibleWord, { [s.sensibleWordHovered]: isVisible })}>{children}</span>
         </Tooltip>
     );
 }
@@ -72,7 +131,7 @@ function Form({ onSubmit }: { onSubmit: () => void }) {
     const [language, setLanguage] = useState<Lang | undefined>();
     const [textType, setTextType] = useState<TextType | undefined>();
     const [text, setText] = useState(
-        'I conceptualize this process as white fragility. Though white fragility is triggered by discomfort and anxiety, it is born of superiority and entitlement. White fragility is not weakness per se. In fact, it is a powerful means of white racial control and the protection of white advantage.'
+        'Led to factors like race, development and location - to who does and who doesn’t make a ‚relatable’ victim of terror. I conceptualize this process as white fragility. Though white fragility is triggered by discomfort and anxiety, it is born of superiority and entitlement. White fragility is not weakness per se. In fact, it is a powerful means of white racial control and the protection of white advantage.'
     );
 
     return (
