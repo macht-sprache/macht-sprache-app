1614116cdd6706319dcf58fc46e45e283a490f8b
Timur Çelikel <me@timurcelikel.com>
2021-09-09 09:48:10 +0200

    TranslationHelpter: add overlay, more dummy content

--- src/TranslationHelperPage/index.tsx
+++ src/TranslationHelperPage/index.tsx
@@ -1,7 +1,7 @@
 import Tooltip from 'rc-tooltip';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import Button, { ButtonContainer } from '../Form/Button';
+import Button, { ButtonContainer, ButtonAnchor } from '../Form/Button';
 import { Select, Textarea } from '../Form/Input';
 import InputContainer from '../Form/InputContainer';
 import Header from '../Header';
@@ -10,14 +10,15 @@ import { SingleColumn } from '../Layout/Columns';
 import { Lang } from '../types';
 import s from './style.module.css';
 import clsx from 'clsx';
+import { ModalDialog } from '../ModalDialog';
 
 type TextType = 'original' | 'translation';
 
 type TranslationList = {
     word: string;
-    definition: string;
+    definition?: string;
     link: string;
-    translations: { word: string; definition: string }[];
+    translations: { word: string; definition?: string }[];
 };
 
 const dummyTranslationRace: TranslationList = {
@@ -36,12 +37,35 @@ const dummyTranslationRace: TranslationList = {
                 'Rassifizierung ist keine direkte Übersetzung für Race. Der Begriff beschreibt den gesellschaftlichen Prozess, der Menschen einer Race zuordnet, also einer bestimmten Identität.',
         },
         {
-            word: 'Race',
+            word: 'Rasse',
             definition:
                 'Rasse ist die wohl bekannteste Übersetzungsoption für Race. Aber die Bedeutung der Begriffe hat historisch unterschiedliche Entwicklungen durchgemacht. Rasse ist beispielsweise eng verbunden mit kolonialer, pseudowissenschaftlicher Rassenforschung und der Nazizeit. Bei Race ist klarer, dass es sich um ein Konstrukt handelt. ',
         },
     ],
 };
+
+const dummyTranslationWhiteFragility: TranslationList = {
+    word: 'white fragility',
+    link: 'https://www.machtsprache.de/term/b2LXNYVQJ3BH5JFDrB8m',
+    translations: [
+        {
+            word: 'Weiße Fragilität',
+        },
+        {
+            word: 'Weiße Empfindlichkeit',
+        },
+        {
+            word: 'Überweisze Empfindlichkeit',
+        },
+        {
+            word: 'Weiße Überempfindlichkeit',
+        },
+        {
+            word: 'White Fragility',
+        },
+    ],
+};
+
 export default function TranslationHelperPage() {
     const [isSubmitted, setIsSubmitted] = useState(false);
 
@@ -70,16 +94,16 @@ export default function TranslationHelperPage() {
 function Analysis({ onCancel }: { onCancel: () => void }) {
     return (
         <>
-            <p>Hover over the words for more information.</p>
+            <p>Click on the words for more information.</p>
             <div className={s.analysisContainer}>
                 <p>
                     Led to factors like <SensibleWord translationList={dummyTranslationRace}>race</SensibleWord>,
                     development and location - to who does and who doesn’t make a ‚relatable’ victim of terror.
                     <br />I conceptualize this process as{' '}
-                    <SensibleWord translationList={dummyTranslationRace}>white fragility</SensibleWord>. Though{' '}
-                    <SensibleWord translationList={dummyTranslationRace}>white fragility</SensibleWord> is triggered by
-                    discomfort and anxiety, it is born of superiority and entitlement.{' '}
-                    <SensibleWord translationList={dummyTranslationRace}>White fragility</SensibleWord>
+                    <SensibleWord translationList={dummyTranslationWhiteFragility}>white fragility</SensibleWord>.
+                    Though <SensibleWord translationList={dummyTranslationWhiteFragility}>white fragility</SensibleWord>{' '}
+                    is triggered by discomfort and anxiety, it is born of superiority and entitlement.{' '}
+                    <SensibleWord translationList={dummyTranslationWhiteFragility}>White fragility</SensibleWord>
                     is not weakness per se. In fact, it is a powerful means of white racial control and the protection
                     of white advantage.
                 </p>
@@ -95,34 +119,71 @@ function Analysis({ onCancel }: { onCancel: () => void }) {
 
 function SensibleWord({ children, translationList }: { children: React.ReactNode; translationList: TranslationList }) {
     const [isVisible, setIsVisible] = useState(false);
+    const [overlayOpen, setOverlayOpen] = useState(false);
 
     const tooltip = (
-        <div className={s.overlay}>
-            <span className={s.overlayWord}>{translationList.word}</span>: {translationList.definition}
-            <h4 className={s.overlayHeading}>Possible translations</h4>
-            <ul className={s.overlayTranslationList}>
-                {translationList.translations.map((translation, index) => (
-                    <li key={index} className={s.overlayTranslation}>
-                        <span className={s.overlayWord}>{translation.word}</span>: {translation.definition}
-                    </li>
-                ))}
-            </ul>
-            <a href={translationList.link}>More information</a>
+        <div className={s.tooltip}>
+            {translationList.definition && <p className={s.tooltipDefinition}>{translationList.definition}</p>}
+            <a href={translationList.link}>Click for possible translations</a>
         </div>
     );
 
     return (
-        <Tooltip
-            overlay={tooltip}
-            placement="bottom"
-            onVisibleChange={visible => {
-                setIsVisible(visible);
-            }}
-            mouseEnterDelay={0.2}
-            mouseLeaveDelay={0.2}
-        >
-            <span className={clsx(s.sensibleWord, { [s.sensibleWordHovered]: isVisible })}>{children}</span>
-        </Tooltip>
+        <>
+            <Tooltip
+                overlay={tooltip}
+                placement="bottom"
+                onVisibleChange={visible => {
+                    setIsVisible(visible);
+                }}
+                mouseEnterDelay={0.2}
+                mouseLeaveDelay={0.2}
+            >
+                <button
+                    onClick={() => {
+                        setOverlayOpen(true);
+                    }}
+                    className={clsx(s.sensibleWord, { [s.sensibleWordHovered]: isVisible })}
+                >
+                    {children}
+                </button>
+            </Tooltip>
+            {overlayOpen && (
+                <ModalDialog
+                    title={translationList.word}
+                    onClose={() => {
+                        setOverlayOpen(false);
+                    }}
+                    isDismissable={true}
+                >
+                    <div className={s.overlay}>
+                        {translationList.definition && (
+                            <p className={s.overlayDefinition}>{translationList.definition}</p>
+                        )}
+                        <ul className={s.overlayTranslationList}>
+                            {translationList.translations.map((translation, index) => (
+                                <li key={index} className={s.overlayTranslation}>
+                                    <span className={s.overlayWord}>{translation.word}</span>
+                                    {translation.definition && <>: </>}
+                                    {translation.definition}
+                                </li>
+                            ))}
+                        </ul>
+                        <ButtonContainer>
+                            <ButtonAnchor href={translationList.link}>More information</ButtonAnchor>
+                            <Button
+                                primary={true}
+                                onClick={() => {
+                                    setOverlayOpen(false);
+                                }}
+                            >
+                                close
+                            </Button>
+                        </ButtonContainer>
+                    </div>
+                </ModalDialog>
+            )}
+        </>
     );
 }
 
@@ -131,7 +192,7 @@ function Form({ onSubmit }: { onSubmit: () => void }) {
     const [language, setLanguage] = useState<Lang | undefined>();
     const [textType, setTextType] = useState<TextType | undefined>();
     const [text, setText] = useState(
-        'Led to factors like race, development and location - to who does and who doesn’t make a ‚relatable’ victim of terror. I conceptualize this process as white fragility. Though white fragility is triggered by discomfort and anxiety, it is born of superiority and entitlement. White fragility is not weakness per se. In fact, it is a powerful means of white racial control and the protection of white advantage.'
+        'Led to factors like race, development and location - to who does and who doesn’t make a ‚relatable’ victim of terror.\n\nI conceptualize this process as white fragility. Though white fragility is triggered by discomfort and anxiety, it is born of superiority and entitlement. White fragility is not weakness per se. In fact, it is a powerful means of white racial control and the protection of white advantage.'
     );
 
     return (
