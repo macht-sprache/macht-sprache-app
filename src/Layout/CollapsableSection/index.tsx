d5903e23657d9fffdd1788c0bbc6d406b8d0f80c
Timur Çelikel <me@timurcelikel.com>
2021-09-21 16:00:08 +0200

    CollapsableSection: add component

---
+++ src/Layout/CollapsableSection/index.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { useDomId } from '../../useDomId';
+import s from './style.module.css';
+
+export default function CollapsableSection({
+    children,
+    title,
+    isOpenDefault = false,
+}: {
+    children: React.ReactNode;
+    title: React.ReactNode;
+    isOpenDefault?: boolean;
+}) {
+    const [isOpen, setIsOpen] = useState(isOpenDefault);
+    const id = useDomId();
+
+    return (
+        <div className={s.container}>
+            <h3 className={s.heading}>
+                <button
+                    onClick={() => {
+                        setIsOpen(!isOpen);
+                    }}
+                    className={s.toggleButton}
+                    aria-expanded={isOpen}
+                    aria-controls={id('section')}
+                >
+                    {title}
+                </button>
+            </h3>
+            {isOpen && (
+                <div id={id('section')} className={s.content}>
+                    {children}
+                </div>
+            )}
+        </div>
+    );
+}
--- src/components/Layout/mdxComponents/index.tsx
+++ src/components/Layout/mdxComponents/index.tsx
@@ -1,10 +1,12 @@
 import Button, { ButtonAnchor, ButtonContainer } from '../../Form/Button';
+import CollapsableSection from '../CollapsableSection';
 import s from './style.module.css';
 
 const components = {
     Button,
     ButtonAnchor,
     ButtonContainer,
+    CollapsableSection,
     p: ({ children }: { children: React.ReactNode }) => <p className={s.paragraph}>{children}</p>,
 };
 
