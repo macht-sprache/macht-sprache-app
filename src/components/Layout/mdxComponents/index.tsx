6690308c89390647ffd4a07e37446b9fe20cfee9
Timur Çelikel <me@timurcelikel.com>
2021-09-21 12:51:15 +0200

    MDX: add Buttons globally

--- src/components/Layout/mdxComponents/index.tsx
+++ src/components/Layout/mdxComponents/index.tsx
@@ -1,6 +1,10 @@
+import Button, { ButtonAnchor, ButtonContainer } from '../../Form/Button';
 import s from './style.module.css';
 
 const components = {
+    Button,
+    ButtonAnchor,
+    ButtonContainer,
     p: ({ children }: { children: React.ReactNode }) => <p className={s.paragraph}>{children}</p>,
 };
 
