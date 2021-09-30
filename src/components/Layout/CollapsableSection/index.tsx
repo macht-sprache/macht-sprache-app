9ca0360cbebdf38cb3cd11ff8d8a079aa9f78066
Timur Çelikel <me@timurcelikel.com>
2021-09-30 14:50:04 +0200

    ExpandableSection: add intro that is always visible

--- src/components/Layout/CollapsableSection/index.tsx
+++ src/components/Layout/CollapsableSection/index.tsx
@@ -1,23 +1,27 @@
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useDomId } from '../../../useDomId';
 import s from './style.module.css';
 
 export default function CollapsableSection({
     children,
     title,
+    intro,
     isOpenDefault,
     domId,
 }: {
     children: React.ReactNode;
     title: React.ReactNode;
+    intro?: React.ReactNode;
     isOpenDefault?: boolean;
     domId?: string;
 }) {
     const [isOpen, setIsOpen] = useState(window.location.hash === `#${domId}` || isOpenDefault);
     const id = useDomId();
+    const { t } = useTranslation();
 
     return (
-        <div className={s.container}>
+        <section className={s.container}>
             <h3 className={s.heading} id={domId}>
                 <button
                     onClick={() => {
@@ -30,11 +34,24 @@ export default function CollapsableSection({
                     {title}
                 </button>
             </h3>
-            {isOpen && (
+            {(isOpen || intro) && (
                 <div id={id('section')} className={s.content}>
-                    {children}
+                    {intro && (
+                        <div className={s.intro}>
+                            {intro}
+                            {!isOpen && (
+                                <>
+                                    {' '}
+                                    <button className={s.moreButton} onClick={() => setIsOpen(true)}>
+                                        {t('common.more')}
+                                    </button>
+                                </>
+                            )}
+                        </div>
+                    )}
+                    {isOpen && <div className={s.children}>{children}</div>}
                 </div>
             )}
-        </div>
+        </section>
     );
 }
