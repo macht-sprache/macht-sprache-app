14ad0f7f7ff983ed3665a68f2a21296ca9c5173a
Timur Çelikel <me@timurcelikel.com>
2021-10-01 10:11:20 +0200

    MdxWrapper: use HashLink for internal links to enable scrolling

--- src/components/MdxWrapper/mdxComponents/index.tsx
+++ src/components/MdxWrapper/mdxComponents/index.tsx
@@ -1,3 +1,4 @@
+import { HashLink } from 'react-router-hash-link';
 import Button, { ButtonAnchor, ButtonContainer } from '../../Form/Button';
 import CollapsableSection from '../../Layout/CollapsableSection';
 import s from './style.module.css';
@@ -8,6 +9,20 @@ const components = {
     ButtonContainer,
     CollapsableSection,
     p: ({ children }: { children: React.ReactNode }) => <p className={s.paragraph}>{children}</p>,
+    a: ({
+        href,
+        ...props
+    }: React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) => {
+        if (href?.startsWith('/')) {
+            return (
+                <HashLink to={href} title={props.title}>
+                    {props.children}
+                </HashLink>
+            );
+        }
+        // eslint-disable-next-line jsx-a11y/anchor-has-content
+        return <a href={href} {...props} />;
+    },
 };
 
 export default components;
