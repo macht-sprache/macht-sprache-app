31c6727dea2a757d6eeb997866f9e696b538bace
Kolja Lange <mail@klange.org>
2021-10-16 17:59:37 +0200

    Package: update typescript

--- src/utils.ts
+++ src/utils.ts
@@ -40,5 +40,5 @@ export function isValidUrl(url: string) {
 
 // https://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
 export function isTouchDevice() {
-    return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 }
