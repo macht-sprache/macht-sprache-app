4375ffcee2e1147f5d42b881e4c8ea95e2a3508d
Kolja Lange <mail@klange.org>
2021-11-08 14:21:55 +0100

    i18n: fix typescript autocomplete & plurals

--- @types/react-i18next/index.d.ts
+++ @types/react-i18next/index.d.ts
@@ -3,6 +3,10 @@ import { resources } from '../../src/i18n/config';
 import { LangA } from '../../src/types';
 
 declare module 'react-i18next' {
-    type DefaultResources = typeof resources[LangA];
-    interface Resources extends DefaultResources {}
+    interface CustomTypeOptions {
+        defaultNS: 'translation';
+        resources: {
+            translation: typeof resources[LangA]['translation'];
+        };
+    }
 }
