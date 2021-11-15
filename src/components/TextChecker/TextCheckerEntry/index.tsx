7137da53da5dd20d5f1604fde705c962f0839911
Timur Çelikel <me@timurcelikel.com>
2021-11-15 09:57:05 +0100

    Text Checker: re-order things, style changes

--- src/components/TextChecker/TextCheckerEntry/index.tsx
+++ src/components/TextChecker/TextCheckerEntry/index.tsx
@@ -1,9 +1,8 @@
 import { useTranslation } from 'react-i18next';
-import { langA, langB } from '../../../languages';
 import { Lang } from '../../../types';
-import Button, { ButtonContainer } from '../../Form/Button';
-import { Select, Textarea } from '../../Form/Input';
+import { Textarea } from '../../Form/Input';
 import InputContainer from '../../Form/InputContainer';
+import BottomBar from '../BottomBar';
 
 export type TextCheckerValue = {
     lang: Lang | undefined;
@@ -23,38 +22,26 @@ export default function TextCheckerEntry({ value: { lang, text }, onChange, onSu
 
     return (
         <>
-            <p>{t('textChecker.entry.description')}</p>
             <InputContainer>
-                <Select
-                    disabled={busy}
-                    label={t('common.langLabels.language')}
-                    value={lang}
-                    onChange={({ target }) => {
-                        if (!target.value) {
-                            updateModel({ lang: undefined });
-                        } else {
-                            updateModel({ lang: target.value === langA ? langA : langB });
-                        }
-                    }}
-                >
-                    <option value=""></option>
-                    <option value={langA}>{t(`common.langLabels.${langA}` as const)}</option>
-                    <option value={langB}>{t(`common.langLabels.${langB}` as const)}</option>
-                </Select>
                 <Textarea
-                    busy={busy}
                     disabled={busy}
                     label={t('textChecker.entry.text')}
                     value={text}
                     minHeight="300px"
                     onChange={({ target }) => updateModel({ text: target.value })}
+                    placeholder={t('textChecker.entry.description')}
+                />
+                <BottomBar
+                    onLanguageChange={lang => {
+                        updateModel({ lang });
+                    }}
+                    buttonLabel={t('textChecker.entry.submit')}
+                    busy={busy}
+                    onSubmit={onSubmit}
+                    buttonDisabled={!lang || text === '' || busy}
+                    lang={lang}
                 />
             </InputContainer>
-            <ButtonContainer>
-                <Button primary={true} disabled={!lang || text === '' || busy} onClick={onSubmit}>
-                    {t('textChecker.entry.submit')}
-                </Button>
-            </ButtonContainer>
         </>
     );
 }
--- src/components/TextChecker/TextCheckerResult/index.tsx
+++ src/components/TextChecker/TextCheckerResult/index.tsx
@@ -5,8 +5,10 @@ import { GetList } from '../../../hooks/fetch';
 import { AddTermPageState } from '../../../pages/AddTermPage';
 import { TERM_ADD } from '../../../routes';
 import { Lang, TermIndex, TextToken, TranslationIndex } from '../../../types';
-import Button, { ButtonContainer } from '../../Form/Button';
+import BlankBox from '../../Form/BlankBox';
+import InputContainer from '../../Form/InputContainer';
 import { SelectTooltipContainer, SelectTooltipLink } from '../../SelectTooltip';
+import BottomBar from '../BottomBar';
 import HighlightedPhrase from './HighlightedPhrase';
 import { useIndexGrouped, useMatchGroups } from './hooks';
 import s from './style.module.css';
@@ -54,25 +56,32 @@ export default function Analysis({ lang, getTermIndex, getTranslationIndex, text
 
     return (
         <>
-            <p>{t(matchGroups.length === 0 ? 'textChecker.result.nothingFound' : 'textChecker.result.description')}</p>
-            <SelectTooltipContainer
-                renderTooltip={selectValue => (
-                    <SelectTooltipLink<AddTermPageState>
-                        to={{ pathname: TERM_ADD, state: { term: selectValue, lang: lang } }}
+            <InputContainer>
+                <BlankBox>
+                    <div className={s.textLabel}>
+                        {t(
+                            matchGroups.length === 0
+                                ? 'textChecker.result.nothingFound'
+                                : 'textChecker.result.description'
+                        )}
+                    </div>
+                    <SelectTooltipContainer
+                        renderTooltip={selectValue => (
+                            <SelectTooltipLink<AddTermPageState>
+                                to={{ pathname: TERM_ADD, state: { term: selectValue, lang: lang } }}
+                            >
+                                {t('textChecker.result.addMissing')}
+                            </SelectTooltipLink>
+                        )}
                     >
-                        {t('textChecker.result.addMissing')}
-                    </SelectTooltipLink>
-                )}
-            >
-                <div className={clsx(s.analysisContainer, { [s.analysisContainerWithTooltip]: tooltipOpen })}>
-                    <p lang={lang}>{children}</p>
-                </div>
-            </SelectTooltipContainer>
-            <ButtonContainer>
-                <Button primary={true} onClick={onCancel}>
-                    {t('textChecker.result.cancel')}
-                </Button>
-            </ButtonContainer>
+                        <div className={clsx(s.analysisContainer, { [s.analysisContainerWithTooltip]: tooltipOpen })}>
+                            <p lang={lang}>{children}</p>
+                        </div>
+                    </SelectTooltipContainer>
+                </BlankBox>
+
+                <BottomBar buttonLabel={t('textChecker.result.cancel')} onSubmit={onCancel} lang={lang} />
+            </InputContainer>
         </>
     );
 }
