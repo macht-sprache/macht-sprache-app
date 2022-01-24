7d662734e456555ca85de08a89e70b0c49619f5d
Kolja Lange <mail@klange.org>
2022-01-24 16:34:36 +0100

    extension: small cleanup

--- src/extension/googleTranslate.tsx
+++ src/extension/googleTranslate.tsx
@@ -1,10 +1,10 @@
 import './setup';
 import ReactDOM from 'react-dom';
+import { TranslationProviderExtension } from '../i18n/config';
+import { useLangCssVars } from '../useLangCssVars';
+import '../vars.css';
 import { Checker } from './Checker';
 import { useGoogleTranslatedEnvironment } from './hooks';
-import { TranslationProviderExtension } from '../i18n/config';
-import '../vars.css';
-import { useLangCssVars } from '../useLangCssVars';
 
 const reactRootElement = document.createElement('div');
 document.body.append(reactRootElement);
@@ -12,13 +12,7 @@ document.body.append(reactRootElement);
 function App() {
     useLangCssVars();
     const { env, onUpdate } = useGoogleTranslatedEnvironment();
-
-    return (
-        <>
-            <Checker env={env} onUpdate={onUpdate} />
-            {/* <TranslationOverlay matches={matches} env={translatorEnv} /> */}
-        </>
-    );
+    return <Checker env={env} onUpdate={onUpdate} />;
 }
 
 ReactDOM.render(
