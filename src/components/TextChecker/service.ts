91725eedb207d2e988532938f81f3baa79e6dfea
Kolja Lange <mail@klange.org>
2022-04-22 14:23:43 +0200

    checker: remove unused methods

--- src/components/TextChecker/service.ts
+++ src/components/TextChecker/service.ts
@@ -1,28 +1,7 @@
-import firebase from 'firebase/compat/app';
 import orderBy from 'lodash/orderBy';
-import { collections } from '../../hooks/data';
-import { useCollection } from '../../hooks/fetch';
 import { langA } from '../../languages';
-import { DocReference, Term, Translation } from '../../types';
+import { Term, Translation } from '../../types';
 import { useLang } from '../../useLang';
-
-export const useTerms = (termRefs: DocReference<Term>[]) =>
-    useCollection(
-        collections.terms.where(
-            firebase.firestore.FieldPath.documentId(),
-            'in',
-            termRefs.map(ref => ref.id)
-        )
-    );
-
-export const useTranslations = (translationRefs: DocReference<Translation>[]) =>
-    useCollection(
-        collections.translations.where(
-            firebase.firestore.FieldPath.documentId(),
-            'in',
-            translationRefs.map(ref => ref.id)
-        )
-    );
 
 export const getLongestEntity = <Entity extends Term | Translation>(terms: Entity[]) =>
     terms.reduce<Entity | undefined>(
