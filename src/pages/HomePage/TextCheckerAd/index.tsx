e62a9d60f3bd1a752c4313a633c4022b01a31366
Timur Çelikel <me@timurcelikel.com>
2022-07-22 17:59:31 +0200

    home page: add browser extension to ad

--- src/pages/HomePage/TextCheckerAd/index.tsx
+++ src/pages/HomePage/TextCheckerAd/index.tsx
@@ -1,16 +1,18 @@
 import { Trans, useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import { ButtonContainer, ButtonLink } from '../../../components/Form/Button';
-import { SingleColumn } from '../../../components/Layout/Columns';
+import { ButtonAnchor, ButtonContainer, ButtonLink } from '../../../components/Form/Button';
+import { FullWidthColumn } from '../../../components/Layout/Columns';
 import { MANIFESTO, TEXT_CHECKER } from '../../../routes';
 import s from './style.module.css';
 import Illustration from './illustration.jpg';
+
+const EXTENSION_LINK = 'https://pocolit.com/2022/03/02/die-macht-sprache-browsererweiterung-ist-da/';
 
 export default function TextCheckerAd() {
     const { t } = useTranslation();
 
     return (
-        <SingleColumn>
+        <FullWidthColumn>
             <div className={s.container}>
                 <h2 className={s.heading}>{t('home.ad.heading')}</h2>
                 <div className={s.body}>
@@ -22,6 +24,8 @@ export default function TextCheckerAd() {
                                 components={{
                                     TextCheckerLink: <Link to={TEXT_CHECKER} />,
                                     ManifestoLink: <Link to={MANIFESTO} />,
+                                    // eslint-disable-next-line jsx-a11y/anchor-has-content
+                                    ExtensionLink: <a href={EXTENSION_LINK} />,
                                 }}
                             />
                         </p>
@@ -29,10 +33,11 @@ export default function TextCheckerAd() {
                         <ButtonContainer>
                             <ButtonLink to={TEXT_CHECKER}>{t('textChecker.title')}</ButtonLink>
                             <ButtonLink to={MANIFESTO}>{t('manifesto.title')}</ButtonLink>
+                            <ButtonAnchor href={EXTENSION_LINK}>Browser Extension</ButtonAnchor>
                         </ButtonContainer>
                     </div>
                 </div>
             </div>
-        </SingleColumn>
+        </FullWidthColumn>
     );
 }
