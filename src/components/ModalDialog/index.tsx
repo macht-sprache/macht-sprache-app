2a97d3269eef6ff9e6dc872d2e0c9a2e019da221
Kolja Lange <mail@klange.org>
2022-08-08 16:46:42 +0200

    modal: replace react-aria with <dialog>
    so it also works in a shadow dom

--- src/components/ModalDialog/index.tsx
+++ src/components/ModalDialog/index.tsx
@@ -1,8 +1,5 @@
-import { useDialog } from '@react-aria/dialog';
-import { FocusScope } from '@react-aria/focus';
-import { useModal, useOverlay, usePreventScroll } from '@react-aria/overlays';
 import clsx from 'clsx';
-import { useRef } from 'react';
+import { ReactEventHandler, useCallback, useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import Logo from '../Layout/logo.svg';
 import { ModalDialogContainer } from './Provider';
@@ -17,49 +14,76 @@ type ModalDialogProps = {
     showLogo?: boolean;
 };
 
-export function ModalDialog({
-    title,
+export function ModalDialog({ title, children, onClose, isDismissable, width = 'medium', showLogo }: ModalDialogProps) {
+    usePreventScroll();
+    const { t } = useTranslation();
+
+    return (
+        <Dialog onClose={onClose} isDismissable={isDismissable} className={s.overlay}>
+            <div className={clsx(s[width], s.overlayInner)}>
+                {showLogo && <img className={s.logo} src={Logo} alt="" />}
+                <header className={s.header}>
+                    <h3 className={s.title}>{title}</h3>
+                    {isDismissable && (
+                        <button className={s.closeButton} aria-label={t('common.formNav.close')} onClick={onClose} />
+                    )}
+                </header>
+                {children}
+            </div>
+        </Dialog>
+    );
+}
+
+function Dialog({
     children,
     onClose,
     isDismissable = true,
-    width = 'medium',
-    showLogo,
-}: ModalDialogProps) {
-    usePreventScroll();
-    const { t } = useTranslation();
-    const ref = useRef<HTMLDivElement>(null);
-    const { overlayProps } = useOverlay({ onClose, isOpen: true, isDismissable }, ref);
-    const { modalProps } = useModal();
-    const { dialogProps, titleProps } = useDialog({}, ref);
+    className,
+}: {
+    children: React.ReactNode;
+    onClose: () => void;
+    isDismissable?: boolean;
+    className?: string;
+}) {
+    const ref = useRef<HTMLDialogElement>(null);
+
+    useEffect(() => {
+        const el = ref.current;
+        el?.showModal();
+        return () => el?.close();
+    }, []);
+
+    const onClick = useCallback<React.MouseEventHandler>(
+        event => {
+            if (isDismissable && event.target === ref.current) {
+                onClose();
+            }
+        },
+        [isDismissable, onClose]
+    );
+
+    const onCancel = useCallback<ReactEventHandler<HTMLDialogElement>>(
+        event => {
+            event.preventDefault();
+            if (isDismissable) {
+                onClose();
+            }
+        },
+        [isDismissable, onClose]
+    );
 
     return (
         <ModalDialogContainer>
-            <div className={s.background}>
-                <FocusScope contain restoreFocus autoFocus>
-                    <div
-                        {...overlayProps}
-                        {...dialogProps}
-                        {...modalProps}
-                        ref={ref}
-                        className={clsx(s[width], s.overlay, { [s.showLogo]: showLogo })}
-                    >
-                        {showLogo && <img className={s.logo} src={Logo} alt="" />}
-                        <header className={s.header}>
-                            <h3 {...titleProps} className={s.title}>
-                                {title}
-                            </h3>
-                            {isDismissable && (
-                                <button
-                                    className={s.closeButton}
-                                    aria-label={t('common.formNav.close')}
-                                    onClick={onClose}
-                                />
-                            )}
-                        </header>
-                        {children}
-                    </div>
-                </FocusScope>
-            </div>
+            <dialog ref={ref} onCancel={onCancel} onClick={onClick} className={className}>
+                {children}
+            </dialog>
         </ModalDialogContainer>
     );
+}
+
+function usePreventScroll() {
+    useEffect(() => {
+        document.documentElement.classList.add(s.noScroll);
+        return () => document.documentElement.classList.remove(s.noScroll);
+    }, []);
 }
