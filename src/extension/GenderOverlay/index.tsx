ce59bebe82b885f1c65605c793dd20cf97cce0ea
Timur Çelikel <me@timurcelikel.com>
2022-08-24 17:36:12 +0200

    extension: add creative solutions to gender overlay

--- src/extension/GenderOverlay/index.tsx
+++ src/extension/GenderOverlay/index.tsx
@@ -5,6 +5,13 @@ import ContentEn from '!babel-loader!@mdx-js/loader!./content.en.mdx';
 import MdxWrapper from '../../components/MdxWrapper';
 import { ModalDialog } from '../../components/ModalDialog';
 import { useLang } from '../../useLang';
+import { useGuidelines } from '../../Manifesto/guidelines/guidelines';
+import CollapsableSection from '../../components/Layout/CollapsableSection';
+import { Suspense } from 'react';
+import { Columns } from '../../components/Layout/Columns';
+import { Trans } from 'react-i18next';
+import { MANIFESTO } from '../../routes';
+import s from './style.module.css';
 
 type Props = {
     isOpen: boolean;
@@ -25,10 +32,49 @@ export function GenderOverlay({ isOpen, onClose }: Props) {
     }
 
     return (
-        <ModalDialog title="What about Gender?" onClose={onClose} showLogo isDismissable>
-            <MdxWrapper>
-                <Content />
-            </MdxWrapper>
+        <ModalDialog title="What about Gender?" onClose={onClose} showLogo isDismissable width="wider">
+            <Columns>
+                <div>
+                    <MdxWrapper>
+                        <Content />
+                    </MdxWrapper>
+                </div>
+                <Suspense fallback={null}>
+                    <Guidelines />
+                </Suspense>
+            </Columns>
         </ModalDialog>
+    );
+}
+
+function Guidelines() {
+    const getGuidelines = useGuidelines(['creative-solutions']);
+    const guidelines = getGuidelines();
+
+    return (
+        <div className={s.manifesto}>
+            <h3>
+                <Trans
+                    i18nKey="textChecker.result.modal.headingGuidelines"
+                    // eslint-disable-next-line jsx-a11y/anchor-has-content
+                    components={{
+                        ManifestoLink: <a target="_blank " href={`${process.env.REACT_APP_MAIN_ORIGIN}${MANIFESTO}`} />,
+                    }}
+                />
+            </h3>
+            {guidelines.map(guideline => {
+                return (
+                    <CollapsableSection
+                        key={guideline.id}
+                        title={guideline.title}
+                        intro={guideline.intro}
+                        domId={guideline.id}
+                        isOpenDefault
+                    >
+                        <guideline.Content />
+                    </CollapsableSection>
+                );
+            })}
+        </div>
     );
 }
