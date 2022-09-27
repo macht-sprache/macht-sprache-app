8f7187ec55a6bed92279ae8110d174bdbd594ade
Timur Çelikel <me@timurcelikel.com>
2022-09-27 12:59:32 +0200

    Logo Bar: add Goethe to separate "partner" section

--- src/pages/HomePage/LogoBar/index.tsx
+++ src/pages/HomePage/LogoBar/index.tsx
@@ -1,10 +1,14 @@
 import s from './style.module.css';
 import PTF from './Logos/PrototypeFund.svg';
 import senat from './Logos/senat_en.svg';
+import goethe from './Logos/goethe.svg';
 import wikimedia from './Logos/wikimedia-de-logo.svg';
 import { useTranslation } from 'react-i18next';
+import React from 'react';
 
-const logos = [
+type Logo = { img: string; label: string };
+
+const logosSupporters: Logo[] = [
     {
         img: PTF,
         label: 'Prototype Fund',
@@ -19,21 +23,37 @@ const logos = [
     },
 ];
 
+const logosPartners: Logo[] = [
+    {
+        img: goethe,
+        label: 'Goethe Institut',
+    },
+];
+
 export function LogoBar() {
     const { t } = useTranslation();
 
     return (
         <section className={s.container}>
             <div className={s.inner}>
-                <h1 className={s.heading}>{t('home.logos')}</h1>
-                <ul className={s.logos}>
-                    {logos.map(({ img, label }) => (
-                        <li className={s.logoWrapper} key={img}>
-                            <img className={s.logo} src={img} alt={label} />
-                        </li>
-                    ))}
-                </ul>
+                <LogoSection title={t('home.logos.supporters')} logos={logosSupporters} />
+                <LogoSection title={t('home.logos.coorporationPartners')} logos={logosPartners} />
             </div>
         </section>
+    );
+}
+
+export function LogoSection({ logos, title }: { logos: Logo[]; title: React.ReactNode }) {
+    return (
+        <div className={s.logoSection}>
+            <h1 className={s.heading}>{title}</h1>
+            <ul className={s.logos}>
+                {logos.map(({ img, label }) => (
+                    <li className={s.logoWrapper} key={img}>
+                        <img className={s.logo} src={img} alt={label} />
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
