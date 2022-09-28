6822bb92c9a5c8287525873781afa66e9523c306
Timur Çelikel <me@timurcelikel.com>
2022-09-28 11:58:11 +0200

    Home: cleanup

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,13 +2,10 @@ import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { useTranslation } from 'react-i18next';
 import { ContentItemList } from '../../components/ContentItemList';
-// import { useUser } from '../../hooks/appContext';
 import { collections } from '../../hooks/data';
 import { useCollection } from '../../hooks/fetch';
 import { useWpPage } from '../../hooks/wp';
 import { ColumnHeading, Columns, FullWidthColumn } from '../../components/Layout/Columns';
-// import { Terms } from '../../components/Terms/TermsSmall';
-// import { TermsWeekHighlights } from '../../components/Terms/TermsWeekHighlights';
 import { WpStyle } from '../../components/WpStyle';
 import { HomePageHeader } from './Header';
 import s from './style.module.css';
@@ -26,11 +23,6 @@ const ABOUT_SLUGS = {
     en: 'about-macht-sprache-short-version-landing-page',
     de: 'ueber-macht-sprache-kurzversion-startseite',
 };
-
-// const EVENT_SLUGS = {
-//     en: 'macht-sprache-events-short-version-landing-page',
-//     de: 'macht-sprache-veranstaltungen-kurzversion-startseite',
-// };
 
 export default function Home() {
     const user = useUser();
@@ -60,9 +52,6 @@ export default function Home() {
                             <ButtonLink primary to={TERMS} size="large">
                                 {t('home.sections.terms.buttonBrowse')}
                             </ButtonLink>
-                            {/* <ButtonLink primary to={REGISTER} size="large">
-                            {t('home.sections.terms.buttonRegister')}
-                        </ButtonLink> */}
                         </>
                     }
                     image={termsAndDiscussion}
@@ -113,55 +102,9 @@ export default function Home() {
                     </section>
                 </FullWidthColumn>
             )}
-            {/* <TermsWeekHighlights /> */}
-            {/* <Columns>
-                <div>
-                    {user ? (
-                        <Suspense fallback={null}>
-                            <LatestActivity />
-                        </Suspense>
-                    ) : (
-                        <>
-                            <About />
-                            <TermsWrapped />
-                            <Events />
-                        </>
-                    )}
-                </div>
-                <div>
-                    {user ? (
-                        <>
-                            <TermsWrapped />
-                            <About />
-                            <Events />
-                        </>
-                    ) : (
-                        <Suspense fallback={null}>
-                            <LatestActivity />
-                        </Suspense>
-                    )}
-                </div>
-            </Columns> */}
         </>
     );
 }
-
-// function TermsWrapped() {
-//     const { t } = useTranslation();
-//     const getTerms = useCollection(collections.terms.where('adminTags.hideFromList', '==', false));
-
-//     return (
-//         <Suspense fallback={null}>
-//             <ColumnHeading>{t('common.entities.term.all')}</ColumnHeading>
-//             <Terms getTerms={getTerms} />
-//             <div className={s.button}>
-//                 <ButtonContainer align="left">
-//                     <ButtonLink to={TERMS}>{t('home.viewAllTerms')}</ButtonLink>
-//                 </ButtonContainer>
-//             </div>
-//         </Suspense>
-//     );
-// }
 
 function About() {
     const getPage = useWpPage(ABOUT_SLUGS);
@@ -181,20 +124,6 @@ function About() {
         </section>
     );
 }
-
-// function Events() {
-//     const getPage = useWpPage(EVENT_SLUGS);
-//     const { t } = useTranslation();
-
-//     return (
-//         <ErrorBoundary fallbackRender={() => null}>
-//             <Suspense fallback={null}>
-//                 <ColumnHeading>{t('home.events')}</ColumnHeading>
-//                 <WpStyle body={getPage().body} />
-//             </Suspense>
-//         </ErrorBoundary>
-//     );
-// }
 
 function LatestActivity() {
     const { t } = useTranslation();
