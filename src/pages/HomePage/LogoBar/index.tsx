e762387ec0136f98eed259aa317f4fd52eb349e6
Timur Çelikel <me@timurcelikel.com>
2022-10-10 10:01:59 +0200

    Homepage: multi lang Senat logo

--- src/pages/HomePage/LogoBar/index.tsx
+++ src/pages/HomePage/LogoBar/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { langA, langB } from '../../../languages';
 import deeptech from './Logos/deeptech.png';
 import goethe from './Logos/goethe.svg';
 import PTF from './Logos/PrototypeFund.svg';
-import senat from './Logos/senat_en.svg';
+import senatEn from './Logos/senat_en.svg';
+import senatDe from './Logos/senat_de.svg';
 import wikimedia from './Logos/wikimedia-de-logo.svg';
 import s from './style.module.css';
+import { useLang } from '../../../useLang';
 
-type Logo = { img: string; label: string };
+type Logo = {
+    img: string | { [langA]: string; [langB]: string };
+    label: string;
+};
 
 const logosSupporters: Logo[] = [
     {
@@ -15,7 +21,10 @@ const logosSupporters: Logo[] = [
         label: 'Prototype Fund',
     },
     {
-        img: senat,
+        img: {
+            [langA]: senatEn,
+            [langB]: senatDe,
+        },
         label: 'Senate Department of Culture and Europe Berlin',
     },
     {
@@ -49,13 +58,20 @@ export function LogoBar() {
 }
 
 export function LogoSection({ logos, title }: { logos: Logo[]; title: React.ReactNode }) {
+    const [lang] = useLang();
+
     return (
         <div className={s.logoSection}>
             <h1 className={s.heading}>{title}</h1>
             <ul className={s.logos}>
                 {logos.map(({ img, label }) => (
-                    <li className={s.logoWrapper} key={img}>
-                        <img className={s.logo} src={img} alt={label} title={label} />
+                    <li className={s.logoWrapper} key={label}>
+                        <img
+                            className={s.logo}
+                            src={typeof img === 'string' ? img : img[lang]}
+                            alt={label}
+                            title={label}
+                        />
                     </li>
                 ))}
             </ul>
